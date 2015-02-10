330656ea90c07f8a189de3e790cdec40d123469b
Sebastian Sarysz <sebsarysz@gmail.com>
2015-02-10 23:50:04 +0100

    Podział

--- web/index.php
+++ web/index.php
@@ -9,70 +9,64 @@
 </head>
 
 <body>
-  <div id="body">
-    <div class="bodyBackground">
-      <div class="bodyWrapper">
-        <div class="mainCol">
+<!-- Styl "pożyczony" z filmweb -->
+<div id="body"><div class="bodyBackground">
+<div class="bodyWrapper"><div class="mainCol">
+<!-- główna zawartość -->
+<div class="pageBox">
 
-              <div class="pageBox">
-                <ul class="nav nav-tabs">
-                  <li class="active">
-                    <a href="#">Filmy ()</a>
-                  </li>
-                  <li>
-                    <a href="/search/serial?q=">Aktorzy ()</a>
-                  </li>
-                  <li>
-                    <a href="/search/serial?q=">Zamowienia ()</a>
-                  </li>
+<?php 
+// załączamy menu
+include 'src/menu.html' 
+?>
 
-                  <li>
-                    <a href="/search/game?q=">Dodaj film</a>
-                  </li>
+<div class="pageBox well filterBox concave">
+  <div id="searchFilters" style="visibility: visible;">
+    <input>
+  </div>
+</div>
 
 
-                  <li>
-                    <a href="/search/person?q=">Dodaj aktora ()</a>
-                  </li>
-                </ul>
+<div class="pageBox">
+  <div class="searchResultInfo boxContainer">
+    <div class="box" id="resultsCount">
+      <strong>Wyniki:</strong> 
 
 
-                <div class="pageBox well filterBox concave">
-                  <div id="searchFilters" style="visibility: visible;">
-                    <input>
-                  </div>
-                </div>
+      <?php 
+      // INFORMACJE ILE WYNIKOW JEST ZNALEZIONYCH
 
-
-                <div class="pageBox">
-                  <div class="searchResultInfo boxContainer">
-                    <div class="box" id="resultsCount">
-                      <strong>Wyniki:</strong> 1 - 10 z 563338
-                    </div>
-
-
-                    <div class="box" id="sortBar">
-                      <label class="first" for=
-                      "search_sortBy"><strong>sortuj</strong> według:</label>
-                      <select class="s" id="search_sort_by" name="sort">
-                        </select> &nbsp; <input checked="checked" id=
-                        "search_sort_desc" name="sortAscending" type="radio"
-                        value="false"> <label for="search_sort_desc">od
-                        największej</label> &nbsp; <input id="search_sort_asc"
-                        name="sortAscending" type="radio" value="true">
-                    </div>
-                  </div>
-                </div>
-
-
-           <!-- KOD -->
-                </div>
-
-
-              </div>
-
-        </div>
-      </div>
+      ?>
     </div>
+
+
+    <div class="box" id="sortBar">
+      <label class="first" for=
+      "search_sortBy"><strong>sortuj</strong> według:</label>
+      <select class="s" name="sort">
+      </select> &nbsp; 
+      <input checked="checked" name="sortAscending" type="radio"
+        value="false"> 
+      <label for="search_sort_desc">od największej</label> &nbsp; 
+      <input name="sortAscending" type="radio" value="true">
+    </div>
+  </div>
+</div>
+
+<?php 
+
+// POKAZ REKORDY
+
+include 'src/show.php';
+?>
+
+<!-- KOD -->
+</div>
+
+
+</div>
+</div>
+</div>
+</div>
 </body>
 </html>
---
+++ web/src/show.php
@@ -0,0 +1,103 @@
+<ul class="sep-hr resultsList">
+  <li>
+    <div class="filmPreview isFilm" id="previewFilmId-862">
+
+    <!-- Zdjęcie -->
+      <div class="filmPoster-2">
+        <a class="fImg2 entityPoster" href="#"><img src="http://placehold.it/140x180"></a>
+      </div>
+
+
+      <div class="filmContent">
+        <h3 class="hdr hdr-big entityTitle"><a class=
+        "filmTitle gwt-filmPage" href=
+        "/Zielona.Mila">Zielona mila</a> <span class=
+        "titleYear">(1999)</span></h3>
+        <span class="filmTime">3 godz. 8 min.</span><br>
+
+
+        <ul class="inline sep-line prevInfo">
+          <li class="filmGenres">
+            <ul class="inline sep-comma">
+              <li>
+                <a href=
+                "/search/film?genreIds=6">Dramat</a>
+              </li>
+            </ul>
+          </li>
+
+
+          <li class="filmCountries">
+            <ul class="inline sep-comma">
+              <li>
+                <a href=
+                "/search/film?countryIds=53">USA</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+
+        <div class="relative prevInfo2">
+          <div class=
+          "well boxContainer va-middle rateInfo hasRate">
+            <div class="box">
+              <span class="rateBox"><i class=
+              "icon-small-voteOn"></i>
+              <strong>8,7<span class=
+              "ten">/10</span></strong>&nbsp;</span>
+
+              <div class="breaker hide">
+              </div>
+            </div>
+
+
+            <div class="box">
+              <i class="icon-small-eye"></i> 60 388
+
+              <div class="breaker hide">
+              </div>
+              ZAKUPIŁO
+            </div>
+
+
+            <div class="box">
+              <a class=
+              "fbtn fbtn-primary pull-right trailerLink nowrap"
+              href="#">KUP FILM</a>
+            </div>
+          </div>
+
+
+          <dl class="filmInfo inline">
+            <dt>Aktorzy:</dt>
+
+
+            <dd>
+              <ul class="inline sep-comma">
+                <li>
+                  <a href="#">Tom Hanks</a>
+                </li>
+
+
+                <li>
+                  <a href="/person/David+Morse-3454"
+                  target="_blank" title="David Morse">David
+                  Morse</a>
+                </li>
+              </ul>
+            </dd>
+          </dl>
+
+
+          <div class="filmPlot">
+            <p>Emerytowany strażnik więzienny opowiada
+            przyjaciółce o niezwykłym mężczyźnie, którego
+            skazano na śmierć za zabójstwo dwóch 9-letnich
+            dziewczynek.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </li>
+</ul>
