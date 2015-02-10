5470c00774609d6bbeee1e0f5cd1fc86ad0390e3
Sebastian Sarysz <sebsarysz@gmail.com>
2015-02-10 23:29:07 +0100

    Podzial strony

--- web/index.php
+++ web/index.php
@@ -1,160 +1,78 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <link href="css/main.css" media="screen" rel="stylesheet" type="text/css">
-    <title></title>
-  </head>
-  <body>
-    <div id="body">
-      <div class="bodyBackground">
-        <div class="bodyWrapper">
-          <div class="mainCol">
-            <div class="mainColWrapper">
-              <form action="?" class="searchInputs" method="get">
+<head>
+  <meta charset="UTF-8">
+  <link href="css/main.css" media="screen" rel="stylesheet" type="text/css">
+
+  <title>
+  </title>
+</head>
+
+<body>
+  <div id="body">
+    <div class="bodyBackground">
+      <div class="bodyWrapper">
+        <div class="mainCol">
+
+              <div class="pageBox">
+                <ul class="nav nav-tabs">
+                  <li class="active">
+                    <a href="#">Filmy ()</a>
+                  </li>
+                  <li>
+                    <a href="/search/serial?q=">Aktorzy ()</a>
+                  </li>
+                  <li>
+                    <a href="/search/serial?q=">Zamowienia ()</a>
+                  </li>
+
+                  <li>
+                    <a href="/search/game?q=">Dodaj film</a>
+                  </li>
+
+
+                  <li>
+                    <a href="/search/person?q=">Dodaj aktora ()</a>
+                  </li>
+                </ul>
+
+
+                <div class="pageBox well filterBox concave">
+                  <div id="searchFilters" style="visibility: visible;">
+                    <input>
+                  </div>
+                </div>
+
+
                 <div class="pageBox">
-                  <ul class="nav nav-tabs">
-                    <li class="active">
-                      <a href="#">Filmy ()</a>
-                    </li>
-                    <li>
-                      <a href="/search/serial?q=">Aktorzy ()</a>
-                    </li>
-                    <li>
-                      <a href="/search/serial?q=">Zamowienia ()</a>
-                    </li>
-                    <li>
-                      <a href="/search/game?q=">Dodaj film</a>
-                    </li>
-                    <li>
-                      <a href="/search/person?q=">Dodaj aktora ()</a>
-                    </li>
-                  </ul>
-                  <div class="pageBox well filterBox concave">
-                    <div id="searchFilters" style="visibility: visible;">
-                      <input>
+                  <div class="searchResultInfo boxContainer">
+                    <div class="box" id="resultsCount">
+                      <strong>Wyniki:</strong> 1 - 10 z 563338
                     </div>
-        
-                  </div>
-                  <div class="pageBox">
-                    <div class="searchResultInfo boxContainer">
-                      <div class="box" id="resultsCount">
-                        <strong>Wyniki:</strong> 1 - 10 z 563338
-                      </div>
-                      <div class="box" id="sortBar">
-                        <label class="first" for=
-                        "search_sortBy"><strong>sortuj</strong> według:</label>
-                        <select class="s" id="search_sort_by" name="sort">
-                          </select> &nbsp; <input checked="checked" id=
-                          "search_sort_desc" name="sortAscending" type="radio"
-                          value="false"> <label for="search_sort_desc">od
-                          największej</label> &nbsp; <input id=
-                          "search_sort_asc" name="sortAscending" type="radio"
-                          value="true">
-                      </div>
-                    </div>
-                  </div>
 
-                  <div id="searchResult">
-                    <div class="pageBox">
-                      <ul class="sep-hr resultsList">
-                        <li id="hit_862">
-                          <div class="filmPreview isFilm" id=
-                          "previewFilmId-862">
-                            
-                            <div class="filmPoster-2">
-                              <a class="fImg2 entityPoster" href="#"><img alt=
-                              "Zielona mila (1999)" src=
-                              "http://1.fwcdn.pl/po/08/62/862/7517878.2.jpg"></a>
-                            </div>
-                            
-      
-                            <div class="filmContent">
-                              <h3 class="hdr hdr-big entityTitle">
-                                <a class="filmTitle gwt-filmPage" href=
-                                "/Zielona.Mila">Zielona mila</a> <span class=
-                                "titleYear">(1999)</span>
-                              </h3>
-                              <span class="filmTime">3 godz. 8 min.</span>
-                              <br/>
-                              <ul class="inline sep-line prevInfo">
-                                <li class="filmGenres">
-                                  <ul class="inline sep-comma">
-                                    <li>
-                                      <a href=
-                                      "/search/film?genreIds=6">Dramat</a>
-                                    </li>
-                                  </ul>
-                                </li>
-                                <li class="filmCountries">
-                                  <ul class="inline sep-comma">
-                                    <li>
-                                      <a href=
-                                      "/search/film?countryIds=53">USA</a>
-                                    </li>
-                                  </ul>
-                                </li>
-                              </ul>
 
-                              <div class="relative prevInfo2">
-                                <div class=
-                                "well boxContainer va-middle rateInfo hasRate">
-                                  <div class="box">
-                                    <span class="rateBox"><i class=
-                                    "icon-small-voteOn"></i>
-                                    <strong>8,7<span class="ten">/10</span></strong>&nbsp;</span>
-                                    <div class="breaker hide"></div>
-                                  </div>
-                                  <div class="box">
-                                    <i class="icon-small-eye"></i> 60 388
-                                    <div class="breaker hide"></div> ZAKUPIŁO
-                                  </div>
-                                  <div class="box">
-                                    <a class="fbtn fbtn-primary pull-right trailerLink nowrap"
-                                    href="#"></i>KUP FILM</a>
-                                  </div>
-                                </div>
-                                <dl class="filmInfo inline">
-         
-                                    Aktorzy:
-                                  </dt>
-                                  <dd>
-                                    <ul class="inline sep-comma">
-                                      <li>
-                                        <a href="#">Tom Hanks</a>
-                                      </li>
-                                      <li>
-                                        <a href="/person/David+Morse-3454"
-                                        target="_blank" title=
-                                        "David Morse">David Morse</a>
-                                      </li>
-                                    </ul>
-                                  </dd>
-                                </dl>
-                                <div class="filmPlot">
-                                  <p>
-                                    Emerytowany strażnik więzienny opowiada
-                                    przyjaciółce o niezwykłym mężczyźnie,
-                                    którego skazano na śmierć za zabójstwo
-                                    dwóch 9-letnich dziewczynek.
-                                  </p>
-                                </div>
-                              </div>
- 
-                            </div>
-        
-                          </div>
-                        </li>
-                      </ul>
+                    <div class="box" id="sortBar">
+                      <label class="first" for=
+                      "search_sortBy"><strong>sortuj</strong> według:</label>
+                      <select class="s" id="search_sort_by" name="sort">
+                        </select> &nbsp; <input checked="checked" id=
+                        "search_sort_desc" name="sortAscending" type="radio"
+                        value="false"> <label for="search_sort_desc">od
+                        największej</label> &nbsp; <input id="search_sort_asc"
+                        name="sortAscending" type="radio" value="true">
                     </div>
                   </div>
                 </div>
-              </form>
-      
-            </div>
-          </div>
+
+
+           <!-- KOD -->
+                </div>
+
+
+              </div>
+
         </div>
       </div>
     </div>
-  </body>
+</body>
 </html>
